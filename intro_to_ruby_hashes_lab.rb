6a3b1f4af2b862c4de6891b6772c72368f9c7fc5
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-27 18:41:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,14 +10,17 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {name: "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_generator = {id: 1}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator = {key: "value"}
 end
 
 def read_from_hash(hash, key)
